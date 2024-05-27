52a370874a2c500dd9d17f4ca9b24aaa52e4a7fb
Pilatis <pilatilucas@gmail.com>
2024-05-26 23:47:49 -0300

    uptade project final

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,61 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState } from 'react'
+import './css/App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [typeOfColor, setTypeOfColor] = useState("hex");
+  const [color, setColor] = useState("#000000");
+
+  const [clicked, setClicked] = useState(false);
+
+  const randomColorUtility = (length) => {
+    return Math.floor(Math.random() * length)
+  }
+
+  const handleRandomHexColor = () => {
+    const hex = [1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+    let hexColor = "#";
+
+    for (let i = 0; i < 6; i++) {
+      hexColor += hex[randomColorUtility(hex.length)]
+    }
+
+    setColor(hexColor);
+  }
+
+  const handleRandomRgbColor = () => {
+    const r = randomColorUtility(256);
+    const g = randomColorUtility(256);
+    const b = randomColorUtility(256);
+
+    setColor(`rgb(${r}, ${g}, ${b})`);
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <main className="main-container">
+        <div className="container" style={{ background: color }}>
+      <h1>Generate Random Color</h1>
+      <div className="container-buttons">
+        <button className="button-color" onClick={() => {setTypeOfColor('hex'); setClicked(true)}}>Create HEX Color</button>
+        <button className="button-color" onClick={() => {setTypeOfColor('rgb'); setClicked(true)}}>Create RGB Color</button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+        {clicked && (
+          <button 
+            className="button-color" 
+            onClick={
+              typeOfColor === 'hex' 
+              ? handleRandomHexColor 
+              : handleRandomRgbColor
+            }
+            >
+              Generate Random Color
+            </button>
+          )}
+        <div className="container-title-color">
+          <h3>{typeOfColor === 'hex' ? 'HEX Color' : 'RGB Color'}</h3>
+          <h1>{color}</h1>
+        </div>
+    </div>
+    </main>
   )
 }
 
